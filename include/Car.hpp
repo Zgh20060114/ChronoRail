9bd313c13b1edde6e42919d21416e893d3bfbba6
Zgh20060114 <123386524+Zgh20060114@users.noreply.github.com>
2025-05-02 09:29:00 +0800

    Update Car.hpp

--- include/Car.hpp
+++ include/Car.hpp
@@ -8,36 +8,33 @@
 class Car {
 public:
     Car(int id, std::string type, double weight, double length);
-    Car(const Car& other);  // 拷贝构造函数
-    Car& operator=(const Car& other);  // 拷贝赋值运算符
+    Car(const Car& other);
+    Car& operator=(const Car& other); 
     ~Car() = default;
-    static Car* createSentinel();  // 新增：创建哨兵节点的静态方法
+    static Car* createSentinel(); 
 
-    // Getters
     int getId() const { return id_; }
     const std::string& getType() const { return type_; }
     double getWeight() const { return weight_; }
     double getLength() const { return length_; }
     time_t getTimestamp() const { return timestamp_; }
 
-    // 序列化支持
     nlohmann::json toJson() const;
     static Car fromJson(const nlohmann::json& j);
 
 private:
-    Car(bool isSentinel);  // 新增：私有构造函数，用于创建哨兵节点
+    Car(bool isSentinel);
     
     int id_;
     std::string type_;
     double weight_;
     double length_;
     time_t timestamp_;
-    
-    // 双向链表指针
+    //双向链表指针
     Car* prev_;
     Car* next_;
 
-    friend class Train;  // 允许 Train 类访问私有成员
+    friend class Train;  // Train 类可以访问私有成员
 };
 
 #endif // CHRONORAIL_CAR_HPP 
